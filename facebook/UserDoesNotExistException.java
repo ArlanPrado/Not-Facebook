feb055fb4fab1e16fe6f15c7b6e181d09e74077b
ArlanPrado <arlanjprado@gmail.com>
2020-05-19 12:21:12 -0700

    Delete Not needed Files

--- facebook/UserDoesNotExistException.java
+++
@@ -1,8 +0,0 @@
-package facebook;
-
-public class UserDoesNotExistException extends Exception{
-
-	private static final long serialVersionUID = 1L;
-	public UserDoesNotExistException() {super();}
-	public UserDoesNotExistException(String message) {super(message);}
-}
